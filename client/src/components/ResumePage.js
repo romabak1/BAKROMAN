21fbbff07cb009e74f4b8fdfb2723b9efe9266ca
romabak1 <romabak27@gmail.com>
2023-12-24 23:54:53 +0100

    done

---
+++ client/src/components/ResumePage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ResumePage = () => {
+  return (
+    <div className="resume-container">
+      <h1>Резюме проекту</h1>
+      <div className="ots">
+        <p>
+          <strong>Назва проекту:</strong> Розроблення програмного продукту для роботи з файловою базою даних про прихід товарів на склад підприємства
+        </p>
+        <p>
+          <strong>Виконав:</strong> Бак Роман
+        </p>
+        <p>студент 3 курсу</p>
+        <p>групи 6.04.122.010.21.2</p>
+        <p>факультету ІТ</p>
+        <p>Бак Р.С.</p>
+      </div>
+    </div>
+  );
+};
+
+export default ResumePage;
