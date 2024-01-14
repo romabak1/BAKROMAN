ea3d885f7046a837ffa5cb871a02d55ed4a88bf7
romabak1 <96170927+romabak1@users.noreply.github.com>
2024-01-15 00:09:10 +0100

    .

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,19 +1,22 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { UserContext } from "../context/UserContext";
-import { useContext } from "react";
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
 
 const ProtectedRoute = ({ children }) => {
-    const { user } = useContext(UserContext);
+  const { user, loading } = useContext(UserContext);
 
-    // Перевірка, чи користувач авторизований через контекст користувача
-    if (!user) {
-        // Якщо користувач не авторизований, перенаправити на сторінку входу (логін)
-        return <Navigate to="/login" />;
-    }
-    
-    // Якщо користувач авторизований, відображати запитаний компонент (дітей)
-    return children;
+  if (loading) {
+    // Показати індикатор завантаження поки перевіряється токен
+    return <div>Loading...</div>; // Можна замінити на компонент Loader, якщо він є
+  }
+
+  if (!user) {
+    // Якщо користувач не авторизований і завантаження завершилось, перенаправити на сторінку логіну
+    return <Navigate to="/login" />;
+  }
+
+  // Якщо користувач авторизований, відображати діти (children)
+  return children;
 };
 
 export default ProtectedRoute;
--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,76 +1,54 @@
-import React, { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import React, { createContext, useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
 
-// Створення контексту для користувача
 export const UserContext = createContext();
 
-// Компонент UserProvider, який надає стан та функції для авторизації та виходу користувача
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true); // Стан для відстеження загрузки
 
-  // Функція для входу користувача та оновлення даних користувача
-  const loginUser = async (token) => {
-    localStorage.setItem("token", token);
+  const setUserFromToken = useCallback(async (token) => {
+    setLoading(true); // Початок загрузки
     try {
-      const response = await axios.get(
-        "http://localhost:5001/api/user/verifyToken",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
+      const response = await axios.get('http://localhost:5001/api/user/verifyToken', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       if (response.data.isValid) {
         setUser(response.data.user);
       } else {
         logoutUser();
       }
     } catch (error) {
-      console.error("Помилка при перевірці токену", error);
+      console.error('Помилка при перевірці токену', error);
       logoutUser();
+    } finally {
+      setLoading(false); // Закінчення загрузки
     }
+  }, []);
+
+  const loginUser = async (token) => {
+    localStorage.setItem('authToken', token);
+    setUserFromToken(token);
   };
 
-  // Функція для виходу користувача
   const logoutUser = () => {
-    localStorage.removeItem("token");
+    localStorage.removeItem('authToken');
     setUser(null);
   };
 
-  // Перевірка токену при завантаженні сторінки
   useEffect(() => {
-    const verifyUser = async () => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        try {
-          const response = await axios.get(
-            "http://localhost:5001/api/user/verifyToken",
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      setUserFromToken(token);
+    } else {
+      setLoading(false); // Закінчення загрузки, якщо токена немає
+    }
+  }, [setUserFromToken]);
 
-          if (response.data.isValid) {
-            setUser(response.data.user);
-          } else {
-            logoutUser();
-          }
-        } catch (error) {
-          console.error("Помилка при перевірці токену", error);
-          logoutUser();
-        }
-      }
-    };
-
-    verifyUser();
-  }, []);
-
-  // Постачання стану та функцій авторизації та виходу в контекст для використання в додатку
   return (
-    <UserContext.Provider value={{ user, setUser, loginUser, logoutUser }}>
+    <UserContext.Provider value={{ user, setUser, loginUser, logoutUser, loading }}>
       {children}
     </UserContext.Provider>
   );
